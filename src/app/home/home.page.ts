beb10a307540bae2a3d7d2cd4981b04cd72ca6d4
saranyapat boonngam <saranyapa1234t@gmail.com>
2024-06-26 14:03:43 +0700

    final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,10 +8,39 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private page1 : NavController) {}
+  weight ? : number;
+  hight ? : number;
+  result ? : number;
+  result_text ? : string="";
 
-  GoToLogin() {
-    this.page1.navigateForward('/login')
+  constructor() {}
+
+  oncal(){
+   if(this.weight == undefined || this.hight == undefined){
+    alert("โปรดใส่ข้อมูลให้ครบถ้วน")
+    return
+   }
+    var hi = this.hight / 100;
+    this.result = this.weight / (hi * hi)
+
+
+
+      if(this.result < 18.50){
+          this.result_text = "อยู่ในเกณฑ์น้ำหนักน้อย//ผอม";
+      }else if(this.result >=18.50 && this.result <= 22.90){
+         this.result_text = "อยู่ในเกณฑ์ปกติ";
+      }else if(this.result >= 23 && this.result <= 24.90){
+          this.result_text = "น้ำหนักเกิน";
+      }else if(this.result >= 25 && this.result <= 29.90){
+         this.result_text = "โรคอ้วนระยะที่ 1"
+      }else{
+        this.result_text = "โรคอ้วนระยะที่ 2"
+      }
+
+
   }
+
+
+
 
 }
